5f7f8d6a301d87dfad53ff0f8b0ef06ad09443fa
mcourter <evilfrenchie666@gmail.com>
2022-04-18 08:59:57 -0400

    Added start of terraform code

---
+++ main.tf
@@ -0,0 +1,54 @@
+#configure the azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus2"
+
+  tags = {
+    Team = "EF"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+
+resource "azurerm_service_plan" "svcplan" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.linux_web_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.svcplan.location
+  service_plan_id     = azurerm_service_plan.svcplan.id
+  site_config {
+      
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "MeRG"
+}
+
+variable "container_registry_name" {
+  default = "MeACR"
+}
+
+variable "service_plan_name" {
+  default = "MeSvcPlan"
+}
+
+variable "linux_web_app_name" {
+  default = "MagicEdenViz"
+}
